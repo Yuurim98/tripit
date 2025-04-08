6f0a696c993da5720a15559f3885f195fe5d69b3
yurim <y1551463@gmail.com>
2025-04-08 18:41:49 +0900

    Initial commit for TripIt backend

---
+++ tripit-backend/src/main/java/com/tripit/tripit_backend/TripitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tripit.tripit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripitBackendApplication.class, args);
+	}
+
+}
